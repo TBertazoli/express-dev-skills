463a461eea2dffe8672a32eb5ff98a36afa5f1b4
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-03-26 16:26:10 -0500

    created skills db

---
+++ models/skills.js
@@ -0,0 +1,19 @@
+const skills = [
+  { name: "HTML", completed: true },
+  { name: "CSS", completed: true },
+  { name: "JavaScript", completed: true },
+  { name: "Node.js", completed: false },
+  { name: "Express", completed: false },
+  { name: "React", completed: false },
+  { name: "MongoDB", completed: false },
+  { name: "Python", completed: false },
+  { name: "Django", completed: false },
+  { name: "SQL", completed: false },
+  { name: "PostgreSQL", completed: false },
+  { name: "MySQL", completed: true },
+  { name: "Git", completed: true },
+  { name: "Postman", completed: false },
+  { name: "Heroku", completed: false },
+];
+
+module.exports = skills;

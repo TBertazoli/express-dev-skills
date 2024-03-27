ee9fbc50bcaaa610fc04a498034a610e9536de3b
Tatiana Bertazoli <tatianabertazoli@gmail.com>
2024-03-27 18:22:49 -0500

    finished show function

--- models/skill.js
+++ models/skill.js
@@ -1,22 +1,19 @@
-const { v4: uuidv4 } = require("uuid");
-const newId = uuidv4();
-
 const skills = [
-  { id: newId, name: "HTML", completed: true },
-  { id: newId, name: "CSS", completed: true },
-  { id: newId, name: "JavaScript", completed: true },
-  { id: newId, name: "Node.js", completed: false },
-  { id: newId, name: "Express", completed: false },
-  { id: newId, name: "React", completed: false },
-  { id: newId, name: "MongoDB", completed: false },
-  { id: newId, name: "Python", completed: false },
-  { id: newId, name: "Django", completed: false },
-  { id: newId, name: "SQL", completed: false },
-  { id: newId, name: "PostgreSQL", completed: false },
-  { id: newId, name: "MySQL", completed: true },
-  { id: newId, name: "Git", completed: true },
-  { id: newId, name: "Postman", completed: false },
-  { id: newId, name: "Heroku", completed: false },
+  { id: 1, name: "HTML", completed: true },
+  { id: 2, name: "CSS", completed: true },
+  { id: 3, name: "JavaScript", completed: true },
+  { id: 4, name: "Node.js", completed: false },
+  { id: 5, name: "Express", completed: false },
+  { id: 6, name: "React", completed: false },
+  { id: 7, name: "MongoDB", completed: false },
+  { id: 8, name: "Python", completed: false },
+  { id: 9, name: "Django", completed: false },
+  { id: 10, name: "SQL", completed: false },
+  { id: 11, name: "PostgreSQL", completed: false },
+  { id: 12, name: "MySQL", completed: true },
+  { id: 13, name: "Git", completed: true },
+  { id: 14, name: "Postman", completed: false },
+  { id: 15, name: "Heroku", completed: false },
 ];
 
 module.exports = {
@@ -25,7 +22,7 @@ module.exports = {
 };
 
 function getOne(id) {
-  return skills.find((s) => s.id === parseInt(id));
+  return skills.find((skill) => skill.id === parseInt(id));
 }
 
 function getAll() {
